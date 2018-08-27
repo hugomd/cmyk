05b5b17ad9d330d88686affac033f749555fdd64
Hugo <h@hugo.sx>
2018-08-27 19:15:28 +1000

    Add a simple timer

---
+++ plugins/timer/index.js
@@ -0,0 +1,44 @@
+const ms = require('ms');
+const config = require('../../config');
+const BasePlugin = require('../plugin-base.js');
+
+class Timer extends BasePlugin {
+	constructor() {
+		super();
+		this.conf = {
+			name: 'timer',
+			help: `\`${config.DISCORD_PREFIX}timer 5m [your message here]\` pings the channel when the timer is up`,
+			regex: new RegExp(`^${config.DISCORD_PREFIX}timer`),
+			react: true
+		};
+	}
+
+	async handler(msg) {
+		super.handler(msg);
+		const [duration, ...message] = this.args;
+
+		if (!duration) {
+			msg.author.send('You need to supply a duration. Format defined here: https://github.com/zeit/ms#examples');
+		}
+
+		const durationMs = ms(duration, {long: true});
+
+		if (!durationMs) {
+			msg.author.send('Duration in invalid format.');
+		}
+
+		setTimeout(async () => {
+			try {
+				if (message.length <= 0) {
+					await msg.reply(`Your timer for ${ms(durationMs, {long: true})} is up!`, {reply: msg.author});
+				} else {
+					await msg.reply(`Your timer for ${ms(durationMs, {long: true})} is up!\n\n\`${message.join(' ')}\``, {reply: msg.author});
+				}
+			} catch (err) {
+				console.log(err);
+			}
+		}, durationMs);
+	}
+}
+
+module.exports = Timer;
