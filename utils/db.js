ea403407ad46d149b09439699661b88cd76a5839
Hugo <h@hugo.sx>
2020-05-07 18:01:57 +1000

    Remove unused dependencies
    - Sequelize

--- utils/db.js
+++
@@ -1,5 +0,0 @@
-const Sequelize = require('sequelize');
-const {POSTGRES_DATABASE, POSTGRES_USERNAME, POSTGRES_PASSWORD} = require('../config');
-
-// eslint-disable-next-line no-unused-vars
-const db = new Sequelize(POSTGRES_DATABASE, POSTGRES_USERNAME, POSTGRES_PASSWORD);
